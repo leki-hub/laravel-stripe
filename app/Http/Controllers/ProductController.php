22706cca259cf6b26ec0d35912c12ff94b9568d9
Leki <lekisintole82@gmail.com>
2024-09-19 11:19:46 +0300

    route file

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,9 +2,16 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\Product;
+use Illuminate\Http\Request ;
+
 
 class ProductController extends Controller
 {
+    public function index(Request $request){
+        $products =Product::all();
+        return view('product.index',compact('products'));
+
+    }
     //
 }
