7862441eaa7933c76e2a4afb274b20da1202535e
Leki <lekisintole82@gmail.com>
2024-09-20 10:56:47 +0300

    checkout route registration

--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,4 @@ use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/',[ProductController::class,'index']);
+Route::post('/checkout',[ProductController::class,'checkout'])->name('checkout');
