b246aadb70b22a9b315b120cd17cbcd1531648e3
Leki <lekisintole82@gmail.com>
2024-09-20 11:18:50 +0300

    session setup

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -16,25 +16,39 @@ class ProductController extends Controller
     }
     public function checkout()
     {
+        $products = Product::all();
+        $lineItems = [];
+
+        foreach($products as $product) {
+            $lineItems[] = [
+                'line_items' => [
+                    [
+                        'price_data' => [
+                            'currency' => 'usd',
+                            'product_data' => [
+                                'name' => $product->name,
+                                'images' => [$product->image],
+                            
+                            ],
+                            'unit_amount' => $product->price*100,
+                        ],
+                        'quantity' => 1,
+                    ]
+                ]
+            ];
+        }
+        
         // Set Stripe secret key
         Stripe::setApiKey(env('STRIPE_SECRET'));
         $session = new \Stripe\StripeClient('sk_test_4eC39HqLyjWDarjtT1zdp7dc');
 
         $session->checkout->sessions->create([
-            'line_items' => [
-                [
-                    'price_data' => [
-                        'currency' => 'usd',
-                        'product_data' => ['name' => 'T-shirt'],
-                        'unit_amount' => 2000,
-                    ],
-                    'quantity' => 1,
-                ],
-            ],
+            'line_items' => $lineItems,
             'mode' => 'payment',
             'success_url' => 'http://localhost:4242/success.html',
             'cancel_url' => 'http://localhost:4242/cancel.html',
         ]);
         return redirect($session->url);
     }
+    
 }
