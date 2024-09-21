40d1193380078a4fbb4437fc593652fa65749e8b
Leki <lekisintole82@gmail.com>
2024-09-21 11:34:45 +0300

    webhook route registration

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/',[ProductController::class,'index']);
 Route::post('/checkout',[ProductController::class,'checkout'])->name('checkout');
+Route::get('/success',[ProductController::class,'success'])->name('checkout.sucess');
+Route::get('/cancel',[ProductController::class,'cancel'])->name('checkout.cancel');
+Route::post('/webhook',[ProductController::class,'webhook'])->name('checkout.webhook');
